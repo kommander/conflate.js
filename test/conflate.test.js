03413b853f593adbd0a037bef1e720ecd629b97e
Sebastian Herrlinger <sebastian@formzoo.com>
2013-05-24 01:34:11 +0200

    Release commit

---
+++ test/conflate.test.js
@@ -0,0 +1,39 @@
+/**
+ * Tests
+ */
+
+var expect = require('expect.js');
+var conflate = require(__dirname + '/../lib/conflate');
+
+describe('Conflate', function(){
+
+  //
+  //
+  it('should simply merge two objects', function(){
+    var x = { a: 1 };
+    var y = { b: '0', c: { d: null } };
+
+    conflate(x, y);
+
+    expect(x.b).to.be('0');
+    expect(x).to.have.property('c');
+    expect(x.c).to.have.property('d', null);
+  });
+
+  //
+  //
+  it('should simply merge multiple objects', function(){
+    var x = { a: 1 };
+    var y = { b: '0', c: { d: null } };
+    var z = { e: '1' };
+
+    conflate(x, y, z);
+
+    expect(x.b).to.be('0');
+    expect(x).to.have.property('c');
+    expect(x.c).to.have.property('d', null);
+    expect(x).to.have.property('e', '1');
+  });
+
+
+});
